380f7972fe1369f9ff92519669ef44079c017805
loserStar <362527240@qq.com>
2019-07-30 18:05:49 +0800

    加入对调用jodd的json工具类的测试

--- src/com/loserstar/controller/TestController.java
+++ src/com/loserstar/controller/TestController.java
@@ -8,11 +8,16 @@
 package com.loserstar.controller;
 
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 import com.loserstar.config.annotation.Controller;
+import com.loserstar.utils.db.jfinal.vo.VResult;
+import com.loserstar.utils.json.LoserStarJsonUtil;
 
 /**
  * author: loserStar
@@ -22,13 +27,39 @@ import com.loserstar.config.annotation.Controller;
 @Controller(controllerKey= {"/test"})
 public class TestController extends BaseController {
 
+	/**
+	 * 测试数据库连接
+	 */
 	public void index() {
 		List<Record> list =  Db.find("select * from DOC_GER_LIST");
 		renderJson(list);
 	}
 	
+	/**
+	 * 测试freemarker渲染
+	 */
 	public void indexPage() {
 		setAttr("name", "loserStar");
 		renderFreeMarker("/index.ftl");
+	}
+	
+	/**
+	 * 测试jodd的json工具类可用
+	 */
+	public void testLoserStarJson() {
+		VResult result = new VResult();
+		try {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("aaa", "111");
+			data.put("bbb", 222);
+			data.put("ccc", 3.333);
+			data.put("ddd", new Date());
+			result.setData(data);
+			result.ok("测试成功，该数据使用的是loserStarJsonUtil序列化");
+			System.out.println(LoserStarJsonUtil.toJsonDeep(result));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		renderText(LoserStarJsonUtil.toJsonDeep(result));
 	}
 }
